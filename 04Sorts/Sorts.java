54ff6b000539a51ba1d5523f8f1b6e7490505f7e
vivienlee00 <vivien.lee.601@gmail.com>
2016-12-14 21:15:11 -0500

    woooooo kinda stupid but IT WORKSgit status

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -24,23 +24,38 @@ public class Sorts{
 
     }
     }
-
+    
     public static void insertionSort(int[] data){
 	int correctindex = 0;
 	for (int i = 1; i < data.length; i++){
-	    int current = i - 1;
-	    while(data[current] > data[i]){
-		current--;
-	    }
-	    correctindex = current;
-	    int temp = data[correctindex];
-	    
-	    data[correctindex] = data[i];
-	    
-	}
+	    if (data[i-1] > data[i]){
+
+		int current = i - 1;
+		while((data[current] > data[i]) && current > 0){
+		    current--;
+		}
+
+		if(data[current] > data[i]){
+		correctindex = current;
+		}
+		else{
+		    correctindex = current + 1;
+		}
 		
+		int temp = data[i];
+		
+		for(int p = i; p > correctindex; p--){
+		    data[p] = data[p - 1];
+		}
+		data[correctindex] = temp;	
+	    }
 	}
+	
     }
+  //  0   1   2   3   4
+   // 64, 25, 12, 22, 11;
+   // 25, 64, 12, 22, 11;
+   // 12, 25, 64, 22, 11;
 
     public static String toString(int[] data) {
     String str = "[";
@@ -53,15 +68,20 @@ public class Sorts{
     public static void main(String[] args){
     int[] x = {64,25,12,22,11};
     int[] y = {100, 0, 10};
+    int[] z = {0, 100, 10, 50};
 
     System.out.println(toString(x));
-    selectionSort(x);
+    insertionSort(x);
     System.out.println(toString(x));
 
 
     System.out.println(toString(y));
-    selectionSort(y);
+    insertionSort(y);
     System.out.println(toString(y));
+
+    System.out.println(toString(z));
+    insertionSort(z);
+    System.out.println(toString(z));
     }
 
 
